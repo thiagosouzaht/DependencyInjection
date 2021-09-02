ad57ca1073ba4dc1daf86c32a8c8fd91e6081567
thiagosouza.ht@hotmail.com <thiagosouza.ht@hotmail.com>
2021-09-01 22:41:43 -0300

    First commit

---
+++ Coffee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Coffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var BrewedCoffe = new BrewedCoffe();
+            var ExpressoCoffee = new ExpressoCoffe();
+            var Cappuccino = new Cappuccino();
+            
+            var Human = new Human(ExpressoCoffee);
+            IHosted
+            Human.MakeCoffe();
+        }
+    }
+}
---
+++ WorkerService/ExpressoCoffe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Coffee
+{
+    public class ExpressoCoffe : ICoffee
+    {
+        public Guid Id { get; set; }
+        
+        private string MachineName = "Máquina de Café Expresso Manual Delonghi EC220 CD -110V";
+        public void MakeCoffe()
+        {
+            this.Id = Guid.NewGuid();  
+            Console.Write($"You have been made an Expresso Coffee id {this.Id} on Machine {MachineName}");
+        }
+    }
+}
